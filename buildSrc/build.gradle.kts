fb2c0333c75ba09c0f3a292dbecc06773167d681
Adam Munro <adjmunro@gmail.com>
2023-09-12 20:20:19 +1200

    feat: add buildSrc

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    gradlePluginPortal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+    implementation("com.android.tools.build:gradle:8.1.1")
+}

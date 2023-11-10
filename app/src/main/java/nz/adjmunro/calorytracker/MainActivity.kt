fe1619f57fb56f9c8596ed20cfdc15b260a394a5
adjmunro <75865923+adjmunro@users.noreply.github.com>
2023-11-10 20:43:38 +1300

    fix: add WelcomeScreen to MainActivity

--- app/src/main/java/nz/adjmunro/calorytracker/MainActivity.kt
+++ app/src/main/java/nz/adjmunro/calorytracker/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import nz.adjmunro.calorytracker.ui.theme.CaloryTrackerTheme
+import nz.adjmunro.onboarding.presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CaloryTrackerTheme {
-
+                WelcomeScreen()
             }
         }
     }

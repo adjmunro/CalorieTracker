1e16f547c8671d2fdb3fc14d5e56e93385b3f985
adjmunro <75865923+adjmunro@users.noreply.github.com>
2023-11-17 07:03:35 +1300

    fix: lower min sdk for test phone

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -1,7 +1,7 @@
 object ProjectConfig {
     const val appId = "nz.adjmunro.calorietracker"
     const val compileSdk = 34
-    const val minSdk = 30
+    const val minSdk = 28
     const val targetSdk = 34
     const val versionCode = 1
     const val versionName = "1.0.0"

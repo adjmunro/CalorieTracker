2ffcc3341c77ddd3d353872c2067482ef2e5e1c3
adjmunro <75865923+adjmunro@users.noreply.github.com>
2023-11-19 19:47:12 +1300

    feat: add TrackedFoodItem component

--- tracker/presentation/src/main/java/nz/adjmunro/tracker/presentation/overview/TrackerOverviewScreen.kt
+++ tracker/presentation/src/main/java/nz/adjmunro/tracker/presentation/overview/TrackerOverviewScreen.kt
@@ -1,19 +1,25 @@
 package nz.adjmunro.tracker.presentation.overview
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import nz.adjmunro.core.util.UiEvent.Navigate
 import nz.adjmunro.coreui.LocalSpacing
 import nz.adjmunro.tracker.presentation.components.NutrientsHeader
+import nz.adjmunro.tracker.presentation.overview.components.AddButton
 import nz.adjmunro.tracker.presentation.overview.components.DaySelector
 import nz.adjmunro.tracker.presentation.overview.components.ExpandableMeal
+import nz.adjmunro.tracker.presentation.overview.components.TrackedFoodItem
 
 @Composable
 fun TrackerOverviewScreen(
@@ -61,7 +67,29 @@ fun TrackerOverviewScreen(
             modifier = Modifier.fillMaxWidth(),
             onToggleClicked = { onEvent(TrackerOverviewEvent.OnToggleMealClicked(meal)) },
         ) {
-
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = LocalSpacing.current.small_8)
+            ) {
+                state.trackedFoods.forEach { food ->
+                    TrackedFoodItem(
+                        trackedFood = food,
+                        onDeleteClicked = { onEvent(TrackerOverviewEvent.OnDeleteTrackedFoodClicked(food)) },
+                    )
+                    Spacer(modifier = Modifier.height(LocalSpacing.current.medium_16))
+                }
+                AddButton(
+                    text = stringResource(
+                        id = nz.adjmunro.core.R.string.add_meal,
+                        meal.name.asString(LocalContext.current),
+                    ),
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        onEvent(TrackerOverviewEvent.OnAddFoodClicked(meal))
+                    },
+                )
+            }
         }
     }
 }
---
+++ tracker/presentation/src/main/java/nz/adjmunro/tracker/presentation/overview/components/TrackedFoodItem.kt
@@ -0,0 +1,137 @@
+package nz.adjmunro.tracker.presentation.overview.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
+import nz.adjmunro.core.util.UiText
+import nz.adjmunro.coreui.LocalSpacing
+import nz.adjmunro.tracker.domain.model.TrackedFood
+import nz.adjmunro.tracker.presentation.components.NutrientInfo
+
+@Composable
+fun TrackedFoodItem(
+    trackedFood: TrackedFood,
+    modifier: Modifier = Modifier,
+    onDeleteClicked: () -> Unit = {},
+) = Row(
+    modifier = modifier
+        .clip(shape = RoundedCornerShape(5.dp))
+        .padding(all = LocalSpacing.current.tiny_4)
+        .shadow(
+            elevation = 1.dp,
+            shape = RoundedCornerShape(5.dp)
+        )
+        .background(color = MaterialTheme.colors.surface)
+        .padding(end = LocalSpacing.current.medium_16)
+        .height(100.dp),
+    horizontalArrangement = Arrangement.SpaceBetween,
+    verticalAlignment = Alignment.CenterVertically,
+) {
+    Image(
+        painter = rememberImagePainter(data = trackedFood.imageUrl,
+                                       builder = {
+                                           crossfade(true)
+                                           error(nz.adjmunro.core.R.drawable.ic_burger)
+                                           fallback(nz.adjmunro.core.R.drawable.ic_burger)
+                                       }),
+        contentDescription = trackedFood.name,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxHeight()
+            .aspectRatio(1f)
+            .clip(
+                shape = RoundedCornerShape(
+                    topStart = 5.dp,
+                    bottomStart = 5.dp
+                )
+            ),
+    )
+    Spacer(modifier = Modifier.width(LocalSpacing.current.medium_16))
+    Column(
+        modifier = Modifier.weight(1f),
+    ) {
+        Text(
+            text = trackedFood.name,
+            style = MaterialTheme.typography.body1,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 2,
+        )
+        Spacer(modifier = Modifier.height(LocalSpacing.current.tiny_4))
+        Text(
+            text = stringResource(
+                id = nz.adjmunro.core.R.string.nutrient_info,
+                trackedFood.amount,
+                trackedFood.calories,
+            ),
+            style = MaterialTheme.typography.body2,
+        )
+    }
+    Spacer(modifier = Modifier.width(LocalSpacing.current.medium_16))
+    Column(
+        modifier = Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            imageVector = Icons.Default.Close,
+            contentDescription = stringResource(id = nz.adjmunro.core.R.string.delete),
+            modifier = Modifier
+                .align(Alignment.End)
+                .clickable { onDeleteClicked() },
+        )
+        Spacer(modifier = Modifier.height(LocalSpacing.current.tiny_4))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            NutrientInfo(
+                name = stringResource(id = nz.adjmunro.core.R.string.carbs),
+                amount = trackedFood.carbs,
+                unit = UiText.of(value = nz.adjmunro.core.R.string.grams),
+                amountTextSize = 16.sp,
+                unitTextSize = 12.sp,
+                nameTextStyle = MaterialTheme.typography.body2,
+            )
+            Spacer(modifier = Modifier.width(LocalSpacing.current.small_8))
+            NutrientInfo(
+                name = stringResource(id = nz.adjmunro.core.R.string.protein),
+                amount = trackedFood.protein,
+                unit = UiText.of(value = nz.adjmunro.core.R.string.grams),
+                amountTextSize = 16.sp,
+                unitTextSize = 12.sp,
+                nameTextStyle = MaterialTheme.typography.body2,
+            )
+            Spacer(modifier = Modifier.width(LocalSpacing.current.small_8))
+            NutrientInfo(
+                name = stringResource(id = nz.adjmunro.core.R.string.fat),
+                amount = trackedFood.fat,
+                unit = UiText.of(value = nz.adjmunro.core.R.string.grams),
+                amountTextSize = 16.sp,
+                unitTextSize = 12.sp,
+                nameTextStyle = MaterialTheme.typography.body2,
+            )
+        }
+    }
+}

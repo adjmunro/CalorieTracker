91b33b4a40d4d32e034544a245a9b559e375f1de
adjmunro <75865923+adjmunro@users.noreply.github.com>
2024-05-12 22:04:06 +1200

    fix: LazyColumn filled max size, even when empty

--- tracker/presentation/src/main/java/nz/adjmunro/tracker/presentation/search/SearchScreen.kt
+++ tracker/presentation/src/main/java/nz/adjmunro/tracker/presentation/search/SearchScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
@@ -112,7 +113,7 @@ private fun SearchScreen(
         },
     )
     Spacer(modifier = Modifier.height(LocalSpacing.current.medium_16))
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+    LazyColumn(modifier = Modifier.fillMaxWidth().wrapContentHeight()) {
         items(state.trackableFoodList) { food ->
             TrackableFoodItem(
                 trackableFoodUiState = food,
